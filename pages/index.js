8d6bb42a9b4a96be82b566e5717cd4a04e80758b
itzkunal01 <kunal.sharmag12@gmail.com>
2023-10-03 11:14:30 +0530

    adding code

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+// import { Inter } from "next/font/google";
+import Hero from "@/components/Hero";
+// const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
