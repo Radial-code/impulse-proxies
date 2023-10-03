c3180522bca74dfe36a828413714893534c49671
itzkunal01 <kunal.sharmag12@gmail.com>
2023-10-03 12:41:45 +0530

    updating header

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@
 import ImpulseAbout from "@/components/ImpulseAbout";
 import ImpulseFAQ from "@/components/ImpulseFAQ";
 import ImpulseFooter from "@/components/ImpulseFooter";
+import Header from "@/components/common/Header";
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Header />
       {/* <div className="container xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto px-3 relative"></div> */}
       <ImpulseAbout />
       <ImpulseFAQ />
