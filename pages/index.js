5f42a950fbf6bb38a19f53b56f772daf97e35fbd
itzkunal01 <kunal.sharmag12@gmail.com>
2023-10-03 12:51:19 +0530

    updating index code

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-// import Image from "next/image";
-// import { Inter } from "next/font/google";
-// import Hero from "@/components/Hero";
+import Header from "@/components/common/Header";
+import Hero from "@/components/Hero";
 import ImpulseAbout from "@/components/ImpulseAbout";
 import ImpulseFAQ from "@/components/ImpulseFAQ";
 import ImpulseFooter from "@/components/ImpulseFooter";
-import Header from "@/components/common/Header";
-// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
       {/* <div className="container xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto px-3 relative"></div> */}
       <ImpulseAbout />
       <ImpulseFAQ />
