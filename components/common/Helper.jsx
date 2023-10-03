63c40e1ce6eb31af1c9eb41535a89150f979667c
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-10-03 16:13:38 +0530

    Our Proxies

--- components/common/Helper.jsx
+++ components/common/Helper.jsx
@@ -61,7 +61,6 @@ export const Accordian = [
     para: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur",
   },
 ];
-<<<<<<< HEAD
 export const CardData = [
   {
     id: 1,
@@ -84,10 +83,26 @@ export const CardData = [
 
 ];
 
-=======
 export const CurrentData = [
   { id: 1, number: "3,234,199", current: "Current Active IPs" },
   { id: 2, number: "1,500+s", current: "Loyal Impulse Users" },
   { id: 3, number: "20,434 GB", current: "Current Proxy Usage" },
 ];
->>>>>>> 93363d31e06d8b3585e863e075a875dbd90a49bd
+export const OurProxies = [
+  {
+    id: 1,
+    heading: "ISP Proxies",
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+  },
+  {
+    id: 2,
+    heading: "Datacenter Proxies",
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+  },
+  {
+    id: 3,
+    heading: "Residential Proxies",
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+  },
+
+];
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import ImpulseAbout from "@/components/ImpulseAbout";
 import ImpulseFAQ from "@/components/ImpulseFAQ";
 import ImpulseFooter from "@/components/ImpulseFooter";
+import OurProxies from "@/components/OurProxies";
 import WhyToChooseUs from "@/components/WhyToChooseUs";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <Hero />
       < Current/>
       <WhyToChooseUs/>
+      <OurProxies/>
       {/* <div className="container xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto px-3 relative"></div> */}
       <ImpulseAbout />
       <ImpulseFAQ />
