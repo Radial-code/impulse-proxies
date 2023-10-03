b7850338b6c275c66b81ed909a5e2a4198575ff3
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-10-03 13:50:43 +0530

    nothing

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Hero from "@/components/Hero";
 import ImpulseAbout from "@/components/ImpulseAbout";
 import ImpulseFAQ from "@/components/ImpulseFAQ";
 import ImpulseFooter from "@/components/ImpulseFooter";
+import WhyToChooseUs from "@/components/WhyToChooseUs";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <WhyToChooseUs/>
       {/* <div className="container xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto px-3 relative"></div> */}
       <ImpulseAbout />
       <ImpulseFAQ />
