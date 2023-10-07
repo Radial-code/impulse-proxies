5140995a02f676d3b6a5e3281eb3d7ed2ca30dda
sukhvinder-jaat <monudhaka829@gmail.com>
2023-10-07 15:51:00 +0530

    dashboar ui

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,13 +1,15 @@
 import Dashboard1 from "@/components/dashborad/Dashboard1";
+import Dashboard2 from "@/components/dashborad/Dashboard2";
 import Dashboard3 from "@/components/dashborad/Dashboard3";
 import DashboardThree from "@/components/dashborad/DashboardThree";
 
 const dashboard = () => {
   return (
     <div>
-      <Dashboard1 />
+      <Dashboard2 />
+      {/* <Dashboard1 />
       <Dashboard3 />
-      <DashboardThree />
+      <DashboardThree /> */}
     </div>
   );
 };
