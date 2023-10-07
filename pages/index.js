23c81f960f5164c2f4bc4c0ee8cb7fe4d0308f13
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-10-07 15:11:59 +0530

    error solove

--- pages/index.js
+++ pages/index.js
@@ -8,28 +8,20 @@ import WhyToChooseUs from "@/components/WhyToChooseUs";
 import BackToTopButton from "@/components/common/BackToTopButton ";
 import Preloader from "@/components/common/Preloader";
 import PageMataTag from "@/components/PageMataTag";
-import Dashboard5 from "@/components/dashborad/Dashboard5";
-import DashboardThree from "@/components/dashborad/DashboardThree";
-import Dashboard1 from "@/components/dashborad/Dashboard1";
-import Dashboard3 from "@/components/dashborad/Dashboard3";
 export default function Home() {
   return (
     <>
       <div className="overflow-hidden">
-        {/* <BackToTopButton /> */}
-        {/* <Preloader /> */}
-        {/* <PageMataTag /> */}
-        {/* <Hero /> */}
-        {/* <Current /> */}
-        {/* <WhyToChooseUs /> */}
-        {/* <OurProxies /> */}
-        {/* <ImpulseAbout /> */}
-        {/* <ImpulseFAQ /> */}
-        {/* <ImpulseFooter /> */}
-        <Dashboard1 />
-        <Dashboard3/>
-        <DashboardThree/>
-        <Dashboard5/>
+        <BackToTopButton />
+        <Preloader />
+        <PageMataTag />
+        <Hero />
+        <Current />
+        <WhyToChooseUs />
+        <OurProxies />
+        <ImpulseAbout />
+        <ImpulseFAQ />
+        <ImpulseFooter />
       </div>
     </>
   );
