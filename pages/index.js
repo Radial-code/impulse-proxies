2106cf0ac5a64db2607bf6d874ba88bf746d3911
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-10-07 14:21:18 +0530

    Dashboard 5 ui done

--- pages/index.js
+++ pages/index.js
@@ -8,20 +8,24 @@ import WhyToChooseUs from "@/components/WhyToChooseUs";
 import BackToTopButton from "@/components/common/BackToTopButton ";
 import Preloader from "@/components/common/Preloader";
 import PageMataTag from "@/components/PageMataTag";
+import Dashboard5 from "@/components/dashborad/Dashboard5";
+import DashboardThree from "@/components/dashborad/DashboardThree";
 export default function Home() {
   return (
     <>
       <div className="overflow-hidden">
-        <BackToTopButton />
-        <Preloader />
-        <PageMataTag />
-        <Hero />
-        <Current />
-        <WhyToChooseUs />
-        <OurProxies />
-        <ImpulseAbout />
-        <ImpulseFAQ />
-        <ImpulseFooter />
+        {/* <BackToTopButton /> */}
+        {/* <Preloader /> */}
+        {/* <PageMataTag /> */}
+        {/* <Hero /> */}
+        {/* <Current /> */}
+        {/* <WhyToChooseUs /> */}
+        {/* <OurProxies /> */}
+        {/* <ImpulseAbout /> */}
+        {/* <ImpulseFAQ /> */}
+        {/* <ImpulseFooter /> */}
+        <DashboardThree/>
+        <Dashboard5/>
       </div>
     </>
   );
