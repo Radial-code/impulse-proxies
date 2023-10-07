da1109bbb6952b688a81a605a0f88a7c71dadd51
sukhvinder-jaat <monudhaka829@gmail.com>
2023-10-07 15:59:37 +0530

    dashboard ui in progress

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -6,10 +6,10 @@ import DashboardThree from "@/components/dashborad/DashboardThree";
 const dashboard = () => {
   return (
     <div>
+      <Dashboard1 />
       <Dashboard2 />
-      {/* <Dashboard1 />
       <Dashboard3 />
-      <DashboardThree /> */}
+      <DashboardThree />
     </div>
   );
 };
