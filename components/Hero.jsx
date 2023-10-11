575902c6bfe2e08b3869ea6cf2cfda58b094cbcb
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-10-11 10:45:00 +0530

    error solved

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
           <div className="container xl:max-w-[1220px] 2xl:max-w-[1320px] mx-auto xl:px-3 px-4 h-full flex items-center">
             <div className="flex items-center justify-between h-full flex-wrap w-full lg:flex-nowrap lg:pt-[70px] lg:pb-[90px]">
               <div className="md:w-6/12 xl:w-6/12 lg:w-6/12 w-full">
-                <h1 className="lg:text-8xl sm:text-[42px] text-[32px] lg:tracking-[-1.16px] sm:tracking-[-0.84px] tracking-[-0.64px] text-white lg:leading-[68px] sm:leading-[54px] leading-[42px] font-bold sm:max-w-[340px] max-w-[290px] lg:max-w-[468px]  sm:w-full">
+                <h1 className="lg:text-8xl sm:text-[42px] text-[32px] lg:tracking-[-1.16px] sm:tracking-[-0.84px] tracking-[-0.64px] text-white lg:leading-[60px] xl:leading-[68px] sm:leading-[54px] leading-[42px] font-bold sm:max-w-[340px] max-w-[290px] lg:max-w-[500px]  sm:w-full">
                   Elevate your <span className="text-cyan-green">success </span>
                   with our Proxies
                 </h1>
@@ -32,12 +32,12 @@ const Hero = () => {
                 </div>
               </div>
               <div className=" md:w-6/12 xl:w-7/12 lg:w-6/12 w-full flex lg:flex-row flex-col justify-center md:justify-end relative after:contents-[''] after:h-[500px] after:w-[450px] after:absolute bgHeroImg after:opacity-30 after:rounded-full after:blur-[137px] after:pointer-events-none after:top-[-50%]">
-                <div className="box_showdow absolute max-w-[456px] w-full h-[542px] blur-[140px] rounded-[36px] opacity-30 top-10 right-7 z-[-1] hidden lg:inline-block"></div>  
+                <div className="box_showdow absolute max-w-[456px] w-full h-[542px] blur-[140px] rounded-[36px] opacity-30 top-10 right-7 z-[-1] hidden lg:inline-block"></div>
                 <Lottie
                   animationData={animationData}
-
                   loop={true}
-                  className="pointer-events-none relative z-50 2xl:h-[500px] sm:max-w-[400px] md:max-w-[370px] lg:max-w-[500px] mx-auto xl:h-[500px] xl:max-w-[600px] sm:mt-0 mt-14 xl:w-full"/>
+                  className="pointer-events-none relative z-50 2xl:h-[500px] sm:max-w-[400px] md:max-w-[370px] lg:max-w-[500px] mx-auto xl:h-[500px] xl:max-w-[600px] sm:mt-0 mt-14 xl:w-full"
+                />
                 <div className="md:hidden sm:flex sm:mb-0 items-center justify-center text-center">
                   <button className=" hover:bg-transparent transition-all max-w-[300px] w-full sm:max-w-[197px] min-h-[55px] border-[2px] border-cyan-green bg-[#163D51] rounded-[100px] font-Montserrat font-semibold text-2xl leading-[120%] text-white mt-10">
                     Purchase
