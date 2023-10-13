6b1fbb81fb0f64468d79a5c7c01b1766cce86d52
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-10-13 15:06:36 +0530

    dashboard errors

--- components/dashborad/ResidentialBoxOne.jsx
+++ components/dashborad/ResidentialBoxOne.jsx
@@ -5,33 +5,33 @@ const ResidentialBoxOne = () => {
   return (
     <>
       <div className="sm:w-6/12 w-full xl:ps-7 sm:ps-2 sm:mt-0 mt-10">
-        <div className="w-full bg-cyan-green py-3.5 px-4 rounded-lg mb-3.5">
-          <p className=" font-Montserrat text-sm text-[#050628] font-bold">
+        <div className="w-full bg-cyan-green py-[14px] px-4 rounded-lg mb-[14px]">
+          <p className=" font-Montserrat text-sm text-[#050628] tracking-[1.1px] leading-normal font-bold">
             RESIDENTIAL
           </p>
         </div>
-        <div className="w-full bg-[#4FDCC7] rounded-lg p-4">
+        <div className="w-full bg-[#4FDCC7] rounded-lg p-[14px]">
           <div className="bg-[#7BF3E1] w-full rounded-lg ps-6 py-2 flex items-center hover:bg-white group transition-all ease-in-out duration-150">
-            <p className=" font-Montserrat transition-all ease-in-out duration-150 text-2xl font-bold text-[#268375] mb-0 group-hover:text-black">
+            <p className=" font-Montserrat transition-all ease-in-out duration-150 text-2xl leading-normal font-bold text-[#268375] mb-0 group-hover:text-black">
               1 GB
             </p>
-            <p className="text-[#35A091] transition-all ease-in-out duration-150 font-Montserrat text-sm font-semibold ms-3 mb-0 group-hover:text-[#7F7F86]">
+            <p className="text-[#35A091] transition-all ease-in-out duration-150 font-Montserrat leading-normal text-sm font-semibold ms-3 mb-0 group-hover:text-[#7F7F86]">
               $9.99
             </p>
           </div>
-          <div className="bg-[#7BF3E1] w-full my-3 rounded-lg ps-6 py-2 flex items-center hover:bg-white group transition-all ease-in-out duration-150">
-            <p className=" font-Montserrat transition-all ease-in-out duration-150 text-2xl font-bold text-[#268375] mb-0 group-hover:text-black">
+          <div className="bg-[#7BF3E1] w-full my-[10px] rounded-lg ps-6 py-2 flex items-center hover:bg-white group transition-all ease-in-out duration-150">
+            <p className=" font-Montserrat transition-all ease-in-out duration-150 text-2xl font-bold leading-normal text-[#268375] mb-0 group-hover:text-black">
               1 GB
             </p>
-            <p className="text-[#35A091] transition-all ease-in-out duration-150 font-Montserrat text-sm font-semibold ms-3 mb-0 group-hover:text-[#7F7F86]">
+            <p className="text-[#35A091] transition-all ease-in-out duration-150 font-Montserrat leading-normal text-sm font-semibold ms-3 mb-0 group-hover:text-[#7F7F86]">
               $9.99
             </p>
           </div>
           <div className="bg-[#7BF3E1] w-full rounded-lg ps-6 py-2 flex items-center hover:bg-white group transition-all ease-in-out duration-150">
-            <p className=" font-Montserrat transition-all ease-in-out duration-150 text-2xl font-bold text-[#268375] mb-0 group-hover:text-black">
+            <p className=" font-Montserrat transition-all ease-in-out duration-150 text-2xl font-bold leading-normal text-[#268375] mb-0 group-hover:text-black">
               1 GB
             </p>
-            <p className="text-[#35A091] transition-all ease-in-out duration-150 font-Montserrat text-sm font-semibold ms-3 mb-0 group-hover:text-[#7F7F86]">
+            <p className="text-[#35A091] transition-all ease-in-out duration-150 font-Montserrat text-sm leading-normal font-semibold ms-3 mb-0 group-hover:text-[#7F7F86]">
               $9.99
             </p>
           </div>
@@ -39,7 +39,7 @@ const ResidentialBoxOne = () => {
             <Link
               href="#"
               aria-label="CONTINUE"
-              className="text-[#050628] text-end group text-sm font-bold tracking-[1.1px] font-Montserrat flex items-center mn-0"
+              className="text-[#050628] text-end group text-sm font-bold leading-normal tracking-[1.1px] font-Montserrat flex items-center mn-0"
             >
               CONTINUE
               <svg
