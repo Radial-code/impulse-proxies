6b98dbaa0f549328efc6babf3247306e0d59ac5f
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-10-16 11:23:37 +0530

    error solving

--- components/DropDown.jsx
+++ components/DropDown.jsx
@@ -13,7 +13,7 @@ const DropDown = () => {
 
 
   return (
-    <div className="flex justify-center lg:inline-block lg:justify-start">
+    <div className="flex justify-center lg:inline-block lg:justify-start ">
       <button
         onClick={toggleDropdown}
         className="text-white font-semibold flex items-center leading-[150%] font-Montserrat text-2xl hover:opacity-70 transition-all ease-in-out duration-200 "
@@ -37,7 +37,7 @@ const DropDown = () => {
         </svg>
       </button>
       {isOpen && (
-        <ul className="absolute z-10 w-60 mt-10 lg:mt-2 py-2 bg-[#040426] rounded-lg shadow-lg border-white border ">
+        <ul className="absolute z-20 w-60 mt-10 lg:mt-2 py-2 bg-[#040426] rounded-lg shadow-lg border-white border ">
           {options.map((option) => (
             <li
               key={option}
@@ -54,3 +54,73 @@ const DropDown = () => {
 };
 
 export default DropDown;
+
+// import { useState } from "react";
+
+// const DropDown = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [selectedOption, setSelectedOption] = useState(null);
+
+//   const options = ["ISP Proxies", "Datacenter Proxies", "Residential Proxies"];
+
+//   const toggleDropdown = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   const selectOption = (option) => {
+//     setSelectedOption(option);
+//     setIsOpen(false);
+//   };
+
+//   // Define components for different options
+//   const components = {
+//     "ISP Proxies": () => <div>ISP Proxies Component</div>,
+//     "Datacenter Proxies": () => <div>Datacenter Proxies Component</div>,
+//     "Residential Proxies": () => <div>Residential Proxies Component</div>,
+//   };
+
+//   return (
+//     <div className="flex justify-center lg:inline-block lg:justify-start ">
+//       <button
+//         onClick={toggleDropdown}
+//         className="text-white font-semibold flex items-center leading-[150%] font-Montserrat text-2xl hover:opacity-70 transition-all ease-in-out duration-200 "
+//       >
+//         {selectedOption || "Products"}
+//         <svg
+//           className="ms-1 mb-1"
+//           xmlns="http://www.w3.org/2000/svg"
+//           width="16"
+//           height="17"
+//           viewBox="0 0 16 17"
+//           fill="none"
+//         >
+//           <path
+//             d="M13 8.5L8 13.5L3 8.5"
+//             stroke="white"
+//             strokeWidth="1.5"
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//           />
+//         </svg>
+//       </button>
+//       {isOpen && (
+//         <ul className="absolute z-20 w-60 mt-10 lg:mt-2 py-2 bg-[#040426] rounded-lg shadow-lg border-white border ">
+//           {options.map((option) => (
+//             <li
+//               key={option}
+//               onClick={() => selectOption(option)}
+//               className="block hover:opacity-70 cursor-pointer transition-all ease-in-out duration-200 px-4 my-3 text-white font-semibold tracking-[-0.38px] leading-[150%] font-Montserrat text-2xl"
+//             >
+//               {option}
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//       {selectedOption &&
+//         components[selectedOption] &&
+//         components[selectedOption]()}
+//     </div>
+//   );
+// };
+
+// export default DropDown;
---
+++ pages/Products.js
@@ -0,0 +1,12 @@
+import Proxies from '@/components/Products/Proxies'
+import React from 'react'
+
+const Products = () => {
+  return (
+    <>
+     <Proxies/> 
+    </>
+  )
+}
+
+export default Products
