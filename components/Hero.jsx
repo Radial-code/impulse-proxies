2ee2955c4854aedda76ae99f7920e4bbcdbcfaea
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-10-24 11:47:24 +0530

    hero background lines

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <>
       <Header />
       <div className=" relative">
-        <div className=" hero-section grad_b flex flex-col justify-center lg:py-8 xl:py-0 pt-[30px] sm:pb-[80px] pb-14">
+        <div className="hero-section grad_b flex flex-col justify-center lg:py-8 xl:py-0 pt-[30px] sm:pb-[80px] pb-14">
           <div className="container xl:max-w-[1220px] 2xl:max-w-[1320px] mx-auto xl:px-3 px-4 h-full flex items-center">
             <div className="flex items-cent er justify-between h-full flex-wrap w-full lg:flex-nowrap lg:pt-[85px] lg:pb-[90px]">
               <div className=" min-h-[210px] md:min-h-[300px] md: lg:min-h-[384px] md:w-[56%] xl:w-7/12 lg:w-6/12 w-full">
