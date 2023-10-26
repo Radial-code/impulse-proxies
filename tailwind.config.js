e83f0fd94c1216a91fad7a54b3b1b000533fb79f
itzkunal01 <kunal.sharmag12@gmail.com>
2023-10-26 11:06:54 +0530

    updating css files GLOBAL , HELPER , FOOTER with autoprefixer code , updating comments etc.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // base: [
-    //   // Your base styles here
-    // ],
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
