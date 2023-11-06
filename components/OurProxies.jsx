9d5bfea191e6665db90edecb400d29f08fbf664e
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-11-06 15:30:37 +0530

    rocket image extra css removed

--- components/OurProxies.jsx
+++ components/OurProxies.jsx
@@ -11,20 +11,11 @@ const OurProxies = () => {
         id="proxies"
         className="pt-[52px] lg:pt-[87px] 2xl:pt-28 pb-[130px] md:pb-[170px] lg:pb-[155px] xl:pb-[108px] 2xl:pb-48 bg-[#040426] relative"
       >
-        {/* <Image
-          width={144}
-          height={220}
-          className="proxies-graph-img rocket_img rocket_graph_image01"
-          src="/assets/images/svg/Racketimage.svg"
-          alt="Group_bg"
-          loading="lazy"
-        /> */}
         <Lottie
           animationData={graphLottie}
           loop={true}
           className="proxies-graph-img rocket_img rocket_graph_image01"
         />
-
         <div className="proxies_box h-10 opacity-10 w-full max-w-[1000px] mx-auto absolute top-0 lg:top-[0px] lg:left-[22%]"></div>
         <div className="box_showdow rounded-[40px] opacity-20 blur-[150px] absolute w-full max-w-[1190px] h-[200px] bottom-0 pointer-events-none"></div>
         <div className="container xl:max-w-[1220px] 2xl:max-w-[1320px] mx-auto xl:px-3 px-4 text-white xl:mb-12 custom-margin-3xl relative">
