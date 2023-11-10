a5fe201975c32dcf6fea4d51efb7637fabb93aee
itzkunal01 <kunal.sharmag12@gmail.com>
2023-11-10 11:11:21 +0530

    updating ProductsRangbar code updated

--- components/Products/ProductsRangbar.jsx
+++ components/Products/ProductsRangbar.jsx
@@ -1,3 +1,78 @@
+// import React, { useState, useEffect } from "react";
+
+// const ProductsRangbar = () => {
+//   const [value, setValue] = useState(10);
+
+//   const handleChange = (e) => {
+//     const newValue = e.target.value;
+//     setValue(newValue);
+//     updateRangeStyle(newValue);
+//   };
+
+//   const updateRangeStyle = (newValue) => {
+//     const percentage = (newValue / 512) * 100;
+//     const rangeInput = document.querySelector(".range-input-2");
+
+//     if (rangeInput) {
+//       rangeInput.style.background = `linear-gradient(to right, #4FDCC7 0%, #4FDCC7 ${percentage}%, #3c3c6b ${percentage}%, #206E63 100%)`;
+//     }
+//   };
+
+//   useEffect(() => {
+//     // Set initial styling when the component mounts
+//     updateRangeStyle(value);
+//   }, [value]);
+
+//   const progressStyle = {
+//     width: `${(value / 512) * 100}%`,
+//   };
+
+//   return (
+//     <>
+//       <div className="progress" style={progressStyle}></div>
+//       <div className="range sm:mt-3">
+//         <div className="flex items-start mt-3 justify-between">
+//           <div className="flex justify-between items-start w-full">
+//             <div className="mb-0 w-full">
+//               <div>
+//                 <div
+//                   className="flex relative justify-center items-center hidden h-[20px] w-full mx-auto rounded"
+//                   style={{
+//                     width: `calc(${(Math.min(value, 512) / 512) * 100}% - 4px)`,
+//                     display: `${value < 2 ? `none` : ``}`,
+//                   }}
+//                 ></div>
+
+//                 <input
+//                   type="range"
+//                   min="10"
+//                   max="512"
+//                   value={value}
+//                   onChange={handleChange}
+//                   className="custom-range w-full range-input-2"
+//                 />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="sm:pt-3 pt-2 flex justify-between items-center">
+//           <p className="text-sm font-Montserrat font-medium text-white mb-0">
+//             {value} Proxies
+//           </p>
+
+//           <input
+//             className="amount-range-value-box"
+//             type="number"
+//             placeholder={value}
+//           />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ProductsRangbar;
+
 import React, { useState, useEffect } from "react";
 
 const ProductsRangbar = () => {
@@ -22,6 +97,24 @@ const ProductsRangbar = () => {
     // Set initial styling when the component mounts
     updateRangeStyle(value);
   }, [value]);
+
+  const handleNumberChange = (e) => {
+    const newValue = parseInt(e.target.value, 10);
+    setValue(newValue);
+    updateRangeStyle(newValue);
+
+    // Update the custom range input value
+    const rangeInput = document.querySelector(".range-input-2");
+    if (rangeInput) {
+      rangeInput.value = newValue;
+    }
+  };
+
+  const handleRangeChange = (e) => {
+    const newValue = parseInt(e.target.value, 10);
+    setValue(newValue);
+    updateRangeStyle(newValue);
+  };
 
   const progressStyle = {
     width: `${(value / 512) * 100}%`,
@@ -48,7 +141,7 @@ const ProductsRangbar = () => {
                   min="10"
                   max="512"
                   value={value}
-                  onChange={handleChange}
+                  onChange={handleRangeChange}
                   className="custom-range w-full range-input-2"
                 />
               </div>
@@ -59,10 +152,11 @@ const ProductsRangbar = () => {
           <p className="text-sm font-Montserrat font-medium text-white mb-0">
             {value} Proxies
           </p>
-
           <input
             className="amount-range-value-box"
             type="number"
+            value={value}
+            onChange={handleNumberChange}
             placeholder={value}
           />
         </div>
